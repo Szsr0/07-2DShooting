c07ba0b93ab286a0499366cd27447fe9ccac7a83
Szsr0 <111580382+Szsr0@users.noreply.github.com>
2025-07-10 19:14:39 +0900

    marged enemy.rkt

--- enemy.rkt
+++ enemy.rkt
@@ -2,11 +2,10 @@
 (require 2htdp/image)
 
 (provide enemy-x enemy-y enemy-v
-         initial-enemy draw-enemy next-enemy
-         ENEMY-SIZE hit?)
+         initial-enemy draw-enemy next-enemy)
 
 (define SCENE-WIDTH 400)
-(define ENEMY-SIZE 40)
+(define ENEMY-SIZE 60)
 (define ENEMY (square ENEMY-SIZE "solid" "blue"))
 
 (define (enemy x y v) (list x y v))
@@ -25,17 +24,16 @@
     [else
      (enemy (+ (enemy-x e) (enemy-v e)) (enemy-y e) (enemy-v e))]))
 
+(define (next-enemy e missiles)
+  (cond [(string? e) "none"]
+        [(ormap (lambda (m)
+                  (and (not (string? m))
+                       (< (abs (- (car m) (car e))) (/ ENEMY-SIZE 2))
+                       (< (abs (- (cadr m) (cadr e))) (/ ENEMY-SIZE 2))))
+                missiles)
+         "none"]
+        [else (move-enemy e)]))
+
 (define (draw-enemy e scene)
   (cond [(string? e) scene]
         [else (place-image ENEMY (enemy-x e) (enemy-y e) scene)]))
-
-(define (hit? m e)
-  (and (not (string? m))
-       (not (string? e))
-       (< (abs (- (car m) (enemy-x e))) (/ ENEMY-SIZE 2))
-       (< (abs (- (cadr m) (enemy-y e))) (/ ENEMY-SIZE 2))))
-
-(define (next-enemy e m)
-  (cond [(string? e) "none"]
-        [(not (hit? m e)) (move-enemy e)]
-        [else "none"]))
