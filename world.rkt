d37de12075b8f3cf998e2d77256eaa58de1f766a
Szsr0 <111580382+Szsr0@users.noreply.github.com>
2025-07-10 19:13:42 +0900

    marged world.rkt

--- world.rkt
+++ world.rkt
@@ -3,41 +3,57 @@
          "player.rkt"
          "enemy.rkt"
          "missile.rkt")
+
 (provide initial-world world-draw world-next world-control)
 
-(struct world (me enemy missile score level) #:transparent)
+;; プレイヤー（me）: (list x last-fired-tick)
+;; enemy : 単一
+;; missiles : リスト（複数）
+(struct world (me enemy missiles score level) #:transparent)
 
+;; 初期状態
 (define initial-world
-  (world initial-player initial-enemy "none" 0 1))
+  (world initial-player initial-enemy '() 0 0))
 
-;;レベルアップ処理
-(define (add1-if-multiple score unit level)
-  (if (and (> score 0) (= (remainder score unit) 0))
-      (add1 level)
-      level))
-
+;; 描画関数
 (define (world-draw w)
-  (define base (draw-missile (world-missile w)))
-  (define with-enemy (draw-enemy (world-enemy w) base))
+  (define bg (draw-missiles (world-missiles w)))
+  (define with-enemy (draw-enemy (world-enemy w) bg))
   (define with-player (draw-player (world-me w) with-enemy))
-  (define score-text (text (string-append "Score: " (number->string (world-score w))) 15 "black"))
-  (define level-text (text (string-append "Power: Lv" (number->string (world-level w))) 15 "red"))
-  (place-image level-text 60 20
-    (place-image score-text 60 40
-      with-player)))
+  (define with-score
+    (place-image
+     (text (string-append "Score: " (number->string (world-score w))) 16 "black")
+     60 20 with-player))
+  (place-image
+   (text (string-append "Power: Lv" (number->string (world-level w))) 16 "red")
+   300 20 with-score))
 
-;; 敵撃破時、スコアとレベルを更新
+;; 状態更新（on-tick）
 (define (world-next w)
-  (define m (world-missile w))
-  (define e (world-enemy w))
-  (define is-hit? (hit? m e))
-  (define new-enemy (if is-hit? "none" (next-enemy e m)))
-  (define new-missile (if is-hit? "none" (next-missile m e (world-level w))))
-  (define new-score (if is-hit? (+ (world-score w) 100) (world-score w)));;スコア増加量
-  (define new-level (add1-if-multiple new-score 500 (world-level w)));;レベルアップに必要なスコア
-  (world (world-me w) new-enemy new-missile new-score new-level))
+  (define old-enemy (world-enemy w))
+  ;; level を取得
+  (define lvl (world-level w))
+  ;;　ミサイル移動が先
+  (define moved-missiles (next-missiles (world-missiles w) old-enemy lvl))
+  ;; 移動後のミサイルで敵の更新
+  (define new-enemy (next-enemy old-enemy moved-missiles))
+  (define new-me (update-player (world-me w)))
+  ;; 撃破判定
+  (define defeated? (and (not (string? old-enemy)) (string? new-enemy)))
+  ;; スコアは撃破ごとに100点
+  (define new-score (if defeated? (+ 100 (world-score w)) (world-score w)))
+  ;; レベルはスコアが500ごとに1Lv
+  (define new-level (quotient new-score 500))
+  
+  (world new-me new-enemy moved-missiles new-score new-level))
+
 
 (define (world-control w key)
-  (define-values (new-me new-missile)
-    (control-player (world-me w) (world-missile w) key))
-  (world new-me (world-enemy w) new-missile (world-score w) (world-level w)))
+  (define-values (new-me new-missiles)
+    (control-player (world-me w) (world-missiles w) key))
+
+  (world new-me
+         (world-enemy w)
+         new-missiles
+         (world-score w)
+         (world-level w)))
